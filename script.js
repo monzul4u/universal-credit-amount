9a737f58e723233d3603f095d1b8368b586043c3
monzul4u <monzul4u@gmail.com>
2024-08-20 12:57:08 +0100

    add

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calculateNetUC() {
-  const ucAmount = 2350.93;
+  const ucAmount = 2350;
   const monzulEarnings =
     parseFloat(document.getElementById("monzulEarnings").value) || 0;
   const salmaEarnings =
@@ -14,11 +14,21 @@ function calculateNetUC() {
   // Calculate net UC amount
   const netUC = ucAmount - homePay;
 
-  // Display the net UC amount
-  document.getElementById("netUCAmount").innerText = `£${netUC.toFixed(2)}`;
+  // Sum of Monzul's earnings and Net UC Amount
+  const monzulNetUC = monzulEarnings + netUC;
+
+  // Display the net UC amount and Monzul's earnings + Net UC Amount
+  document.getElementById(
+    "netUCAmount"
+  ).innerText = `Net UC Amount: £${netUC.toFixed(2)}`;
+  document.getElementById(
+    "monzulNetUC"
+  ).innerText = `Total Accumulated Earning: £${monzulNetUC.toFixed(2)}`;
 }
 
 function resetForm() {
   document.getElementById("paymentForm").reset();
-  document.getElementById("netUCAmount").innerText = "£0.00";
+  document.getElementById("netUCAmount").innerText = "Net UC Amount: £0.00";
+  document.getElementById("monzulNetUC").innerText =
+    "Monzul's Earnings + Net UC Amount: £0.00";
 }
